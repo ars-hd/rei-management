b56f6738ba658b82a258467863ca15d3c35734e9
Abdul Rehman Shahid <rehman.virtualassistant@gmail.com>
2023-03-30 18:05:12 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+let PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("200")
+});
+
+
+app.listen(PORT)
+module.exports.app = app;

eaee3ad75e511048596e69bfdb9d60407b13d96a
Abdul Rehman Shahid <rehman.virtualassistant@gmail.com>
2023-03-30 19:10:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { Lead, Client } = require('./models');
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('Error connecting to MongoDB Atlas:', error.message));
-
-const Leades = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true },
-    mobile: { type: String, required: true },
-    address: {type: String, required: true}
-});
-
-const Leads = mongoose.model('Leads', Leades);
-
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let PORT = process.env.PORT || 3000;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected to MongoDB Atlas'))
+.catch((error) => console.error('Error connecting to MongoDB Atlas:', error.message));
 
 app.get('/', (req, res) => {
     res.send("200")
 });
 
-app.post('/add', async (req, res) => {
-    let { first_name, last_name, email, mobile, address } = req.query;
+
+
+app.post('/add_lead', async (req, res) => {
+    let { client, first_name, last_name, email, mobile, address } = req.query;
     try {
-        const lead = new Leads({
+        const lead = new Lead({
+            client: client,
             first_name: first_name,
             last_name: last_name,
             email: email,
@@ -42,16 +36,49 @@ app.post('/add', async (req, res) => {
     } catch (error) {
     res.status(400).send(error.message);
     }
-});
+})
 
 app.get('/leads', async (req, res) => {
     try {
-        const leads = await Leads.find();
+        const leads = await Lead.find({}).where('client').equals(req.query.client);
         res.status(200).send(leads);
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
-app.listen(PORT)
-module.exports = app
+
+app.post('/add_client', async (req, res) => {
+    let { client, first_name, last_name, email, mobile, company } = req.query;
+    try {
+        const client = new Client({
+            client: client,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            mobile: mobile,
+            company: company
+        });
+        await client.save();
+        res.status(201).send(client);
+
+    } catch (error) {
+    res.status(400).send(error.message);
+    }
+});
+
+app.get('/clients', async (req, res) => {
+    try {
+        const clients = await Client.find();
+        res.status(200).send(clients);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;

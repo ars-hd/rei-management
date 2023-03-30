482bad9c7791ac28001b0b61da054fb21dbd4ac1
Abdul Rehman Shahid <rehman.virtualassistant@gmail.com>
2023-03-30 19:40:14 +0500

    Update models.js

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const monggose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
 
+
 monggose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.error('Error connecting to MongoDB Atlas:', error.message));
 

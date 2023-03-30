16cb1932dc84fc4547e0afc4935f44cebf97054d
Abdul Rehman Shahid <rehman.virtualassistant@gmail.com>
2023-03-30 18:08:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((error) => console.error('Error connecting to MongoDB Atlas:', error.message));
+
+const Leades = new mongoose.Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    mobile: { type: String, required: true },
+    address: {type: String, required: true}
+});
+
+const Leads = mongoose.model('Leads', Leades);
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.json());
 
 let PORT = process.env.PORT || 3000;
 
@@ -13,6 +25,32 @@ app.get('/', (req, res) => {
     res.send("200")
 });
 
+app.post('/add', async (req, res) => {
+    let { first_name, last_name, email, mobile, address } = req.query;
+    try {
+        const lead = new Leads({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            mobile: mobile,
+            address: address
+        });
+        await lead.save();
+        res.status(201).send(lead);
+
+    } catch (error) {
+    res.status(400).send(error.message);
+    }
+});
+
+app.get('/leads', async (req, res) => {
+    try {
+        const leads = await Leads.find();
+        res.status(200).send(leads);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 app.listen(PORT)
-module.exports = app;
+module.exports = app

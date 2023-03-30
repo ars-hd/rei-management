47180360c44de09690c8c29dfd58f3e16eaaa262
Abdul Rehman Shahid <rehman.virtualassistant@gmail.com>
2023-03-30 18:10:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
